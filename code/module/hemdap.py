3ba8224dd240e836dab6a5045b8d6cb68a2c12a8
idata <IDATA.health@outlook.com>
2024-10-30 13:51:54 +0800

    算法框架

--- code/module/hemdap.py
+++ code/module/hemdap.py
@@ -8,132 +8,6 @@ import torch
 import random
 
 
-# class Hemdap(nn.Module):
-#     def __init__(self, hidden_dim, feats_dim_list1, feats_dim_list2, feat_drop, attn_drop,
-#                  P1, P2, sample_rate, sample_rate1, nei_num, tau, lam, gamma):
-#         super(Hemdap, self).__init__()
-#         self.hidden_dim = hidden_dim
-#         self.gamma = gamma
-#
-#         # 初始化特征转换层
-#         self.fc_list1 = nn.ModuleList([
-#             nn.Linear(feats_dim, hidden_dim, bias=True)
-#             for feats_dim in feats_dim_list1
-#         ])
-#         self.fc_list2 = nn.ModuleList([
-#             nn.Linear(feats_dim, hidden_dim, bias=True)
-#             for feats_dim in feats_dim_list2
-#         ])
-#         for fc in self.fc_list1:
-#             nn.init.xavier_normal_(fc.weight, gain=1.414)
-#         for fc in self.fc_list2:
-#             nn.init.xavier_normal_(fc.weight, gain=1.414)
-#
-#         # 定义特征丢弃
-#         if feat_drop > 0:
-#             self.feat_drop = nn.Dropout(feat_drop)
-#         else:
-#             self.feat_drop = lambda x: x
-#
-#         # 定义元路径编码器和结构编码器
-#         self.mp1 = Mp_encoder(P1, hidden_dim, attn_drop)
-#         self.sc1 = Sc_encoder(hidden_dim, sample_rate, nei_num, attn_drop)
-#         self.mp2 = Mp_encoder(P2, hidden_dim, attn_drop)
-#         self.sc2 = Sc_encoder(hidden_dim, sample_rate1, nei_num, attn_drop)
-#
-#         self.decoder1 = Mp_decoder(1, hidden_dim, attn_drop)
-#         self.decoder2 = Mp_decoder(1, hidden_dim, attn_drop)
-#
-#         # 定义对比损失
-#         self.contrast = Contrast(hidden_dim, tau, lam)
-#
-#         dropout_rate = 0.5
-#         self.link_pred_mlp = nn.Sequential(
-#             nn.Linear(2*hidden_dim, hidden_dim),
-#             nn.ReLU(),
-#             nn.Dropout(dropout_rate),
-#             nn.Linear(hidden_dim, 1),
-#             # nn.ReLU(),
-#             # nn.Dropout(dropout_rate),
-#             # nn.Linear(hidden_dim // 2, 1),
-#             nn.Sigmoid()
-#         )
-#
-#     def forward(self, feats1, feats2, mps1, mps2, nei_index1, nei_index2):
-#         # Feature transformation
-#         h_all1 = [F.elu(self.feat_drop(self.fc_list1[i](feats1[i]))) for i in range(len(feats1))]
-#         h_all2 = [F.elu(self.feat_drop(self.fc_list2[i](feats2[i]))) for i in range(len(feats2))]
-#
-#         # Masking strategy
-#         masked_feats1, masked_indices1 = self.mask_nodes(h_all1[0])
-#         masked_feats2, masked_indices2 = self.mask_nodes(h_all2[0])
-#
-#         # Generating embeddings for masked nodes
-#         z_masked_mp1 = [self.mp1(masked_feats1, mps1[i]) for i in range(len(mps1))]
-#         z_masked_mp2 = [self.mp2(masked_feats2, mps2[i]) for i in range(len(mps2))]
-#
-#         # Reconstructing the features of masked nodes
-#         reconstructed_feats1 = [self.decoder1(z_masked_mp1[i], mps1[i]) for i in range(len(mps1))]
-#         reconstructed_feats2 = [self.decoder2(z_masked_mp2[i], mps2[i]) for i in range(len(mps2))]
-#
-#         # Compute SCE loss for intra-view contrast (miRNA)
-#         sce_loss_intra_mirna = 0
-#         for i in range(len(mps1)):
-#
-#              sce_loss_intra_mirna += self.sce_loss(reconstructed_feats1[i], h_all1[0])
-#
-#         sce_loss_intra_mirna /= len(mps1) * (len(mps1) - 1) / 2
-#
-#         # Compute SCE loss for intra-view contrast (disease)
-#         sce_loss_intra_disease = 0
-#         for i in range(len(mps2)):
-#
-#             sce_loss_intra_disease += self.sce_loss(reconstructed_feats2[i], h_all1[0])
-#         sce_loss_intra_disease /= len(mps2) * (len(mps2) - 1) / 2
-#
-#         # Total loss
-#         loss = sce_loss_intra_mirna + sce_loss_intra_disease
-#
-#         # 获取元路径和结构编码的嵌入
-#         z_mp1 = self.mp1(h_all1[0], mps1)
-#         z_sc1 = self.sc1(h_all1, nei_index1)
-#         z_mp2 = self.mp2(h_all2[0], mps2)
-#         z_sc2 = self.sc2(h_all2, nei_index2)
-#
-#         return z_mp1, z_sc1,z_mp2,z_sc2, loss
-#
-#     def sce_loss(self, z1, z2, eta=2.0):
-#         # Normalize embeddings
-#         z1_norm = F.normalize(z1, p=2, dim=-1)
-#         z2_norm = F.normalize(z2, p=2, dim=-1)
-#
-#         # Compute cosine similarity
-#         sim = torch.sum(z1_norm * z2_norm, dim=-1)
-#
-#         # Compute SCE loss
-#         loss = torch.mean((1 - sim) ** eta)
-#
-#         return loss
-#
-#     def mask_nodes(self, feats):
-#         num_nodes = feats.size(0)
-#         num_masked = int(num_nodes * 0.15)
-#         masked_indices = torch.randperm(num_nodes)[:num_masked]
-#
-#         masked_feats = feats.clone()
-#         masked_feats[masked_indices] = self.mask_value
-#
-#         return masked_feats, masked_indices
-#
-#
-#     def get_link_prediction(self, embedding_u, embedding_v):
-#         # 将两个节点的嵌入拼接后通过 MLP 预测链接概率
-#
-#         # x = self.film_fusion(embedding_u, embedding_v)
-#         x = torch.cat([embedding_u, embedding_v], dim=1)
-#         prob = self.link_pred_mlp(x)
-#         return prob
-
 class Hemdap(nn.Module):
     def __init__(self, hidden_dim, feats_dim_list1, feats_dim_list2, feat_drop, attn_drop,
                  P1, P2, sample_rate, sample_rate1, nei_num, tau, lam, gamma, mask_ratio1=0.1, mask_ratio2=0.1):
@@ -230,12 +104,12 @@ class Hemdap(nn.Module):
         # 调用知识感知编码器，生成增强表示
         new_h_mirna, new_h_disease, knowledge_loss = self.knowledge_aware_encoder(h_all1[0], h_all2[0], z_mp1_agg,
                                                                                   md_matrix)
-        new_h_mirna1, new_h_disease1, knowledge_loss1 = self.knowledge_aware_encoder(h_all2[0], h_all1[0], z_mp1_agg,
+        new_h_disease1, new_h_mirna1, knowledge_loss1 = self.knowledge_aware_encoder(h_all2[0], h_all1[0], z_mp1_agg,
                                                                                      md_matrix.T)
         # 总损失
         total_loss = contrast_loss + knowledge_loss + knowledge_loss1
 
-        return z_mp1_agg + new_h_mirna + new_h_disease1 + h_all1[0], z_sc1, z_mp2_agg + new_h_mirna1 + new_h_disease + \
+        return z_mp1_agg + new_h_mirna + new_h_mirna1 + h_all1[0], z_sc1, z_mp2_agg + new_h_disease1 + new_h_disease + \
                h_all2[0], z_sc2, total_loss
 
     def get_link_prediction(self, embedding_u, embedding_v):
@@ -301,45 +175,6 @@ class Hemdap(nn.Module):
         return new_h_mirna, new_h_disease, knowledge_loss
 
 
-class NMF(nn.Module):
-    def __init__(self, latent_dim=64, max_iter=500, tol=1e-4):
-        super(NMF, self).__init__()
-        self.latent_dim = latent_dim
-        self.max_iter = max_iter
-        self.tol = tol  # 停止条件的阈值
-
-    def forward(self, md_matrix):
-        """
-        使用非负矩阵分解（NMF）将关联矩阵md_matrix分解为miRNA和疾病的64维潜在因子表示。
-
-        参数:
-        - md_matrix: 关联矩阵，形状 [num_mirna, num_disease]
-
-        返回:
-        - h_mirna: miRNA的64维潜在因子表示，形状 [num_mirna, latent_dim]
-        - h_disease: 疾病的64维潜在因子表示，形状 [latent_dim, num_disease]
-        """
-        num_mirna, num_disease = md_matrix.shape
-        W = torch.rand(num_mirna, self.latent_dim, requires_grad=True, device=md_matrix.device)  # miRNA矩阵
-        H = torch.rand(self.latent_dim, num_disease, requires_grad=True, device=md_matrix.device)  # 疾病矩阵
-
-        optimizer = torch.optim.Adam([W, H], lr=1e-3)
-
-        for iteration in range(self.max_iter):
-            md_reconstructed = torch.matmul(W, H)
-            loss = torch.norm(md_matrix - md_reconstructed, p='fro')
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            if loss.item() < self.tol:
-                break
-
-            W.data = torch.clamp(W.data, min=0)
-            H.data = torch.clamp(H.data, min=0)
-
-        return W, H  # W: [num_mirna, latent_dim], H: [latent_dim, num_disease]
 
 
 class ContrastiveLearning(nn.Module):
@@ -359,19 +194,6 @@ class ContrastiveLearning(nn.Module):
         return sum(losses)
 
 
-# class SemanticAttention(nn.Module):
-#     def __init__(self, hidden_dim):
-#         super(SemanticAttention, self).__init__()
-#         self.project = nn.Sequential(
-#             nn.Linear(hidden_dim, hidden_dim),
-#             nn.Tanh(),
-#             nn.Linear(hidden_dim, 1, bias=False)
-#         )
-#
-#     def forward(self, z):
-#         w = self.project(torch.stack(z, dim=1)).squeeze(-1)
-#         beta = torch.softmax(w, dim=1)
-#         return (torch.stack(z, dim=1) * beta.unsqueeze(-1)).sum(1)
 class SemanticAttention(nn.Module):
     def __init__(self, hidden_dim, attn_drop):
         super(SemanticAttention, self).__init__()
